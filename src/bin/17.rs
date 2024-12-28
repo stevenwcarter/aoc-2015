169790a93186d90b4dc32195817cb1f4ff21604b
Steven Carter <steve@javapl.us>
2024-12-28 11:16:39 -0500

    Removing tests on day 17

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -46,19 +46,19 @@ pub fn part_two(input: &str) -> Option<usize> {
     None
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn test_part_one() {
+//         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+//         assert_eq!(result, None);
+//     }
+//
+//     #[test]
+//     fn test_part_two() {
+//         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+//         assert_eq!(result, None);
+//     }
+// }
