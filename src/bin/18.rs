8faf5cf47ecfa3abb5561b7a75380930a12cf26c
Steven Carter <steve@javapl.us>
2024-12-28 11:46:56 -0500

    Removing tests on day 18

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -127,19 +127,19 @@ pub fn part_two(input: &str) -> Option<usize> {
     Some(lightgrid.count_lights())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn test_part_one() {
+//         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+//         assert_eq!(result, None);
+//     }
+//
+//     #[test]
+//     fn test_part_two() {
+//         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+//         assert_eq!(result, None);
+//     }
+// }
