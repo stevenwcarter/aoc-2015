195325ff614ff419cf06ae890bdebd4724299c01
Steven Carter <steve@javapl.us>
2024-12-30 00:15:26 -0500

    Disabling tests

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -249,15 +249,15 @@ mod tests {
         assert!(simulate_battle(player, boss));
     }
 
-    #[test]
-    fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
+    // #[test]
+    // fn test_part_one() {
+    //     let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+    //     assert_eq!(result, None);
+    // }
+    //
+    // #[test]
+    // fn test_part_two() {
+    //     let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+    //     assert_eq!(result, None);
+    // }
 }
--- src/bin/23.rs
+++ src/bin/23.rs
@@ -155,9 +155,9 @@ mod tests {
         assert_eq!(result, Some(2));
     }
 
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
+    // #[test]
+    // fn test_part_two() {
+    //     let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+    //     assert_eq!(result, None);
+    // }
 }
